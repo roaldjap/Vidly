6b498a0b55852f99057b520cd4ec6067d54f73d3
Roald Ignacio <roaldjap@gmail.com>
2019-12-18 18:45:18 +1300

    Added new custom route with constraints

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -25,5 +25,24 @@ namespace Vidly.Controllers
             // redirect and pass arguments to next page: $page = 1 and sortBy = "name"
             return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
         }
+
+        public ActionResult Edit(int id){
+            return Content("id= " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy) {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBt{1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content( year + "/" + month);
+        }
+
+        
     }
 }
